ecbdfa994e97f9ee868ac1df63a1293049123b8a
ltdagabriel <gabriel_choptian@hotmail.com>
2018-03-22 19:55:13 -0300

    error

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from teste import subtrair
 
 def test_somar():
     """docstring for test_somar"""
-    assert somar(2,4) == 6
+    assert somar(2,4) == 5
 
 def test_subtrair():
     """docstring for test_somar"""
